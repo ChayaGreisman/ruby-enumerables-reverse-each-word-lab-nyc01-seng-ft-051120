f0ad6bf235a17aa22bafc8e23a53eb71c2383421
Chaya Greisman <chayamgreisman@gmail.com>
2020-05-04 03:57:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,5 @@
+
+=begin
 #using .each
 def reverse_each_word(sentence)
   
@@ -28,5 +30,14 @@ def reverse_each_word(sentence)
   
   sentence
 end
+=end
 
+def reverse_each_word(sentence) 
+  reversed_sentence = [] 
+  sentence.split.each do 
+    |word| reversed_word = word.reverse 
+    reversed_sentence << reversed_word 
+    end 
+    reversed_sentence.join(" ")
+end
 
