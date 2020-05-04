0dd0b1b06958fe9a443288fa5d97e29a4b6ab03d
Chaya Greisman <chayamgreisman@gmail.com>
2020-05-04 03:49:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,21 @@
+#using .each
+def reverse_each_word(sentence)
+  
+  sentence = sentence.split
+  
+  sentence.each do |string|
+  string.reverse!
+  
+  
+  end
+  sentence = sentence.join(" ")
+  
+  sentence
+end
+
+
+#using .collect -?? not sure what is supposed to be different, or if something is extra- should the code be  shorter when i use collect??
+
 def reverse_each_word(sentence)
   
   sentence = sentence.split
@@ -12,18 +30,3 @@ def reverse_each_word(sentence)
 end
 
 
-=begin
-def reverse_each_word(sentence)
-  
-  sentence = sentence.split
-  
-  sentence.each do |string|
-  string.reverse!
-  
-  
-  end
-  sentence = sentence.join(" ")
-  
-  sentence
-end
-=end
